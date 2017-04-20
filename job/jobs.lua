05502b13aeceb2b517c771aa5b39fe0257022673
souzen13 <baptiste.franel@outlook.fr>
2017-04-20 10:14:51 +0200

    Create jobs.lua

---
+++ job/jobs.lua
@@ -0,0 +1,71 @@
+-- Edit the table below for more loadouts!
+LOADOUTS =  {
+    ["cop"] = {
+        name = "Cop",
+        permission_level = 3,
+        weapons = { "WEAPON_PISTOL50", "WEAPON_STUNGUN", "WEAPON_NIGHTSTICK", "WEAPON_FLAREGUN" },
+        skins = { "s_m_y_cop_01" }
+    },
+    ["cop2"] = {
+        name = "Cop2",
+        permission_level = 3,
+        weapons = { "WEAPON_PISTOL50", "WEAPON_STUNGUN", "WEAPON_NIGHTSTICK", , "WEAPON_FLAREGUN" },
+        skins = { "csb_cop",
+        "csb_trafficwarden",
+        "ig_trafficwarden"}
+    },
+    ["fbi"] = {
+        name = "FBI",
+        permission_level = 4,
+        weapons = { "WEAPON_PISTOL50", "WEAPON_STUNGUN", "WEAPON_NIGHTSTICK", "WEAPON_PUMPSHOTGUN", "WEAPON_FLAREGUN", "WEAPON_ASSAULTSMG" },
+        skins = { "s_m_y_swat_01" }
+    },
+    
+    ["inspecteur"] = {
+        name = "Inspecteur",
+        permission_level = 5,
+        weapons = { "WEAPON_HEAVYPISTOL", "WEAPON_STUNGUN", "WEAPON_NIGHTSTICK", , "WEAPON_FLAREGUN" }
+        skins = {"s_m_m_ciasec_01" }
+    
+    ["medic"] = {
+        name = "Medic",
+        permissions_level = 2
+        weapons = { "WEAPON_FLAREGUN", "WEAPON_PETROLCAN" },
+        skins = {"s_m_m_paramedic_01",
+            "s_f_y_scrubs_01"
+            }
+    
+    
+    ["random"] = {
+        name = "Random",
+        weapons = { "WEAPON_PETROLCAN" },
+        skins = {"a_m_y_skater_01",
+            "a_m_y_skater_02",
+            "a_m_m_beach_01",
+            "a_m_m_bevhills_01",
+            "a_m_m_bevhills_02",
+            "a_m_m_business_01",
+            "a_m_m_eastsa_01",
+            "a_m_m_eastsa_02",
+            "a_m_m_farmer_01",
+            "a_m_m_genfat_01",
+            "a_m_m_golfer_01",
+            "a_m_m_hillbilly_01",
+            "a_m_m_indian_01",
+            "a_m_m_mexcntry_01",
+            "a_m_m_paparazzi_01",
+            "a_m_m_tramp_01",
+            "a_m_y_hiker_01",
+            "a_m_y_hippy_01",
+            "a_m_y_genstreet_01",
+            "a_m_m_socenlat_01",
+            "a_m_m_og_boss_01",
+            "a_f_y_tourist_02",
+            "a_f_y_tourist_01",
+            "a_f_y_soucent_01",
+            "a_f_y_scdressy_01",
+            "a_m_y_cyclist_01",
+            "a_m_y_golfer_01"
+        }
+    }
+}
